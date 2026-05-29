8f0b06866ef30480fdd5e5c454ce4ae5d55afc18
christina chen <158211764+christinac09@users.noreply.github.com>
2026-05-29 11:46:27 -0400

    this is werid must fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/fonts', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxt/fonts'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+  css: ['~/assets/css/main.css'],
 })
 
 //color palette:
