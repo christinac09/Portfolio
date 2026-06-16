24cc3d95b16265901514d901e5f1f46ab4023354
christina <christinac272@nycstudents.net>
2026-06-16 00:34:04 -0400

    ahhhh

---
+++ app/pages/test.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-black">
+    <h1 class="text-5xl text-green-400 font-bold">Tailwind is working</h1>
+    <div class="btn">button</div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/content',
+    '@nuxt/content','@nuxtjs/tailwindcss'
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
